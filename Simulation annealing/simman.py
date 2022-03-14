57bd6492b0c8269aa84b7b6ef79c10ebcd9e4e3f
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-03-14 20:47:34 +0100

    Update simman.py

--- Simulation annealing/simman.py
+++ Simulation annealing/simman.py
@@ -4,16 +4,17 @@
 """
 import map_min_search as mm
 import numpy as np
+import math
                                   
-num_of_steps = 2000                                 # number of steps: do not change
+num_of_steps = 10                                 # number of steps: do not change
 
 num_of_parameters = 2                               # number of solution parameters
 N = num_of_parameters
 
-T = ???                                             # temperature (randomness coefficient)
-T_min = ???                                         # minimal temperature
-wT = ???                                            # change of temperature
-c = ???                                             # constant due to the influence of T for acceptance probability
+T = 10 # temperature (randomness coefficient)
+T_min = 0.00001 # minimal temperature
+wT = 1 # change of temperature
+c = 1 # constant due to the influence of T for acceptance probability
 
 Solution = np.random.rand(N)*20-10                  # initial solution - random point
 
@@ -22,16 +23,25 @@ E_min = 10e40							            # minimal function value
 E_prev = 0                                          # previous value of the function
 Records = np.empty((0,N))                           # array of record solutions
 
-mm.show_the_point(Solution,"initial solution")
+# mm.show_the_point(Solution,"initial solution")
+
+def GetPointInCircle(r):
+    circle_r, x0, y0 = r, 0, 0
+    alpha = 2 * math.pi * np.random.rand()
+    r = circle_r * math.sqrt(np.random.rand())
+    x = r * math.cos(alpha) + x0
+    y = r * math.sin(alpha) + y0
+    Point = [x,y]
+    return Point
 
 for ep in range(num_of_steps):
-   SolutionNew = ???                                # new solution (should be near previous one !)
+   SolutionNew = Solution + GetPointInCircle(T) # new solution (should be near previous one !)
 
    E = mm.fun3(SolutionNew[0],SolutionNew[1])       # function value for point coordinates
 
    dE = E - E_prev                                  # change of function value (dE < 0 means than new solution is better)
 
-   p_accept = ???                                   # acceptance probability
+   p_accept = 1 + (1 / (math.exp(dE / (c * T))))                                  # acceptance probability
    if np.random.rand() < p_accept:
       Solution = SolutionNew
       E_prev = E
