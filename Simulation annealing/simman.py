3ee2e0e97ff074ed7d734cf3ec91725fa506a0d8
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-03-14 21:11:20 +0100

    Update simman.py

--- Simulation annealing/simman.py
+++ Simulation annealing/simman.py
@@ -6,13 +6,13 @@ import map_min_search as mm
 import numpy as np
 import math
                                   
-num_of_steps = 10                                 # number of steps: do not change 2000
+num_of_steps = 2000                                 # number of steps: do not change 2000
 
 num_of_parameters = 2                               # number of solution parameters
 N = num_of_parameters
 
 T = 10 # temperature (randomness coefficient)
-T_min = 0.00001 # minimal temperature
+T_min = 0.000001 # minimal temperature
 wT = 1 # change of temperature
 c = 2 # constant due to the influence of T for acceptance probability
 
@@ -36,13 +36,20 @@ def GetPointInCircle(r):
 
 for ep in range(num_of_steps):
 
-    print("Step: " + str(ep+1))
     SolutionNew = Solution + GetPointInCircle(T) # new solution (should be near previous one !)
+    while(SolutionNew[0]>10 or SolutionNew[0]<-10 or SolutionNew[1]<-10 or SolutionNew[1]> 10):
+        SolutionNew = Solution + GetPointInCircle(T)
+
     E = mm.fun3(SolutionNew[0],SolutionNew[1])       # function value for point coordinates
     dE = E - E_prev                                  # change of function value (dE < 0 means than new solution is better)
+
     p_accept = 1 + (1 / (math.exp(dE / (c * T))))                                  # acceptance probability
-    print("Actual Point: " + str(Solution) + " New Point: " + str(SolutionNew) + " Value delta: " + str(dE) +
-    " Accept prob: " + str(p_accept))
+    
+    if((ep>995 and ep<=1000) or (ep>1995)):
+        print("Step: " + str(ep+1))
+        print("Actual Point: " + str(Solution) + " New Point: " + str(SolutionNew) + " Value delta: " + str(dE) +
+        " Accept prob: " + str(p_accept))
+        print("New T: "+ str(T))
 
     if np.random.rand() < p_accept:
         Solution = SolutionNew
@@ -55,13 +62,12 @@ for ep in range(num_of_steps):
         Records = np.append(Records, [SolutionNew], axis = 0)
 
     T = (np.random.rand()*10)* wT # temperature changing (can be only after accaptance or in another place)
-    wT -= 0.01
-    print("New T: "+ str(T))
+    wT -= 0.00055
     if T < T_min:
         T = T_min
 # end of steps loop
 
 text = "\nbest solution, value = " + str(E_min) + " for point x1 = " + str(Solution_min[0]) + " x2 = " + str(Solution_min[1])
 print(text + "\n")
-#mm.show_point_sequence(Records,"record sequence, " + text)
+mm.show_point_sequence(Records,"record sequence, " + text)
 
