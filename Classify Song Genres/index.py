c4a43496dfba7b90a9cb10268b2215b26692cca1
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-03-30 22:41:14 +0200

    Update index.py

--- Classify Song Genres/index.py
+++ Classify Song Genres/index.py
@@ -12,3 +12,13 @@ echo_tracks.info()
 # Create a correlation matrix
 corr_metrics = echo_tracks.corr()
 corr_metrics.style.background_gradient()
+
+# Import train_test_split function and Decision tree classifier
+# ... YOUR CODE ...
+from sklearn.model_selection import train_test_split
+# Create features
+features = echo_tracks.drop(["genre_top", "track_id"], axis=1).values
+# Create labels
+labels = echo_tracks["genre_top"].values
+# Split our data
+train_features, test_features, train_labels, test_labels = train_test_split(features,labels,random_state=10)
