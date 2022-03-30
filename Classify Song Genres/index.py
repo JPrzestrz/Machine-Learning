606eb0365bff62cfc2ece587f3c047c452cec4d2
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-03-30 22:40:42 +0200

    Preparing

---
+++ Classify Song Genres/index.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read in track metadata with genre labels
+tracks = pd.read_csv('datasets/fma-rock-vs-hiphop.csv')
+
+# Read in track metrics with the features
+echonest_metrics = pd.read_json('datasets/echonest-metrics.json', precise_float = True)
+
+# Merge the relevant columns of tracks and echonest_metrics
+echo_tracks = echonest_metrics.merge(tracks[['track_id','genre_top']],on='track_id')
+
+# Inspect the resultant dataframe
+echo_tracks.info()
