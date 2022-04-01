929b57d13ac1adfedffac7163dc82336de7618bb
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-04-01 22:46:06 +0200

    Update index.py

--- Machine Learning for Marketing/index.py
+++ Machine Learning for Marketing/index.py
@@ -18,3 +18,13 @@ categorical = telco_raw.nunique()[telco_raw.nunique() < 5].keys().tolist()
 categorical.remove(target[0])
 # Store numerical column names
 numerical = [x for x in telco_raw.columns if x not in custid + target + categorical]
+
+from sklearn.preprocessing import StandardScaler
+# Perform one-hot encoding to categorical variables 
+telco_raw = pd.get_dummies(data = telco_raw, columns = categorical, drop_first=True)
+# Initialize StandardScaler instance
+scaler = StandardScaler()
+# Fit and transform the scaler on numerical columns
+scaled_numerical = scaler.fit_transform(telco_raw[numerical])
+# Build a DataFrame from scaled_numerical
+scaled_numerical = pd.DataFrame(scaled_numerical, columns=numerical)
