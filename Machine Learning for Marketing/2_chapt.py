050cb3221416bd0180c8c1e2bb33d7e189c9d4fb
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-04-05 21:08:06 +0200

    Update 2_chapt.py

--- Machine Learning for Marketing/2_chapt.py
+++ Machine Learning for Marketing/2_chapt.py
@@ -1,12 +1,37 @@
+'''
+----------------------------------------
+Code and data from the Chapter 1 
+----------------------------------------
+'''
 import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split
 telcom = pd.read_csv('telco.csv')
-
 # Store customerID and Churn column names
 custid = ['customerID']
 target = ['Churn']
+# Store categorical column names
+categorical = telcom.nunique()[telcom.nunique() < 5].keys().tolist()
+# Remove target from the list of categorical variables
+categorical.remove(target[0])
+# Store numerical column names
+numerical = [x for x in telcom.columns if x not in custid + target + categorical]
+from sklearn.preprocessing import StandardScaler
+# Perform one-hot encoding to categorical variables 
+telcom = pd.get_dummies(data = telcom, columns = categorical, drop_first=True)
+# Initialize StandardScaler instance
+scaler = StandardScaler()
+# Fit and transform the scaler on numerical columns
+scaled_numerical = scaler.fit_transform(telcom[numerical])
+# Build a DataFrame from scaled_numerical
+scaled_numerical = pd.DataFrame(scaled_numerical, columns=numerical)
+telcom[numerical]=scaled_numerical
 
+'''
+----------------------------------------
+Chapter 2 - Churn prediction and drivers
+----------------------------------------
+'''
 # Print the unique Churn values
 # Calculate the ratio size of each churn group
 # Import the function for splitting data to train and test
