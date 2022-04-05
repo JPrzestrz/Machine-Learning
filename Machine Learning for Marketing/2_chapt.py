09bf65c5282849e348e32e9a59009b6ccf023239
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-04-05 22:03:48 +0200

    Update 2_chapt.py

--- Machine Learning for Marketing/2_chapt.py
+++ Machine Learning for Marketing/2_chapt.py
@@ -3,6 +3,7 @@
 Code and data from the Chapter 1 
 ----------------------------------------
 '''
+from turtle import pos
 import pandas as pd 
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -52,16 +53,8 @@ test_X = test[cols]
 test_Y = test[target]
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score
 logreg = LogisticRegression()
-# Fit logistic regression on training data
-logreg.fit(train_X, train_Y)
-# Predict churn labels on testing data
-pred_test_Y = logreg.predict(test_X)
-# Calculate accuracy score on testing data
-test_accuracy = accuracy_score(test_Y, pred_test_Y)
-# Print test accuracy score rounded to 4 decimals
-print('Test accuracy:', round(test_accuracy, 4))
 
 # Initialize logistic regression instance 
 logreg = LogisticRegression(penalty='l1', C=0.025, solver='liblinear')
@@ -70,4 +63,27 @@ logreg.fit(train_X, train_Y)
 # Predict churn values on test data
 pred_test_Y = logreg.predict(test_X)
 # Print the accuracy score on test data
-print('Test accuracy w. regularization:', round(accuracy_score(test_Y, pred_test_Y), 4))
+print('Coeff:', np.count_nonzero(logreg.coef_))
+print('Test accuracy:', round(accuracy_score(test_Y, pd.DataFrame(pred_test_Y)), 4))
+print('Test recall:', round(recall_score(test_Y, pred_test_Y, pos_label = 'Yes'), 4))
+
+C = [1, .5, .25, .1, .05, .025, .01, .005, .0025]
+l1_metrics = np.zeros((len(C), 5))
+l1_metrics[:,0] = C
+# Run a for loop over the range of C list length
+for index in range(0, len(C)):
+  # Initialize and fit Logistic Regression with the C candidate
+  logreg = LogisticRegression(penalty='l1', C=C[index], solver='liblinear')
+  logreg.fit(train_X, train_Y)
+  # Predict churn on the testing data
+  pred_test_Y = logreg.predict(test_X)
+  # Create non-zero count and recall score columns
+  l1_metrics[index,1] = np.count_nonzero(logreg.coef_)
+  l1_metrics[index,2] = accuracy_score(test_Y, pred_test_Y)
+  l1_metrics[index,3] = precision_score(test_Y, pred_test_Y,pos_label='Yes')    
+  l1_metrics[index,4] = recall_score(test_Y, pred_test_Y,pos_label='Yes')
+
+# Name the columns and print the array as pandas DataFrame
+col_names = ['C','Non-Zero Coeffs','Accuracy','Precision','Recall']
+print(pd.DataFrame(l1_metrics, columns=col_names))
+
