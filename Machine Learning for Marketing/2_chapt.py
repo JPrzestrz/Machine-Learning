a6414828c585db61a7a44ae1d75665d838101144
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-04-12 19:08:52 +0200

    Update 2_chapt.py

--- Machine Learning for Marketing/2_chapt.py
+++ Machine Learning for Marketing/2_chapt.py
@@ -114,3 +114,14 @@ coefficients['Exp_Coefficient'] = np.exp(coefficients['Coefficient'])
 coefficients = coefficients[coefficients['Coefficient']!=0]
 # Print the values sorted by the exponent coefficient
 print(coefficients.sort_values(by=['Exp_Coefficient']))
+
+# Export graphviz object from the trained decision tree 
+exported = tree.export_graphviz(decision_tree=mytree, 
+			# Assign feature names
+      out_file=None, feature_names=train_X.columns, 
+			# Set precision to 1 and add class names
+			precision=1, class_names=['Not churn','Churn'], filled = True)
+# Call the Source function and pass the exported graphviz object
+graph = graphviz.Source(exported)
+# Display the decision tree
+display_image("/usr/local/share/datasets/decision_tree_rules.png")
