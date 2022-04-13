1ff86110c934f02aebe5ffa52120372c43f8ef40
JPrzestrz <JPrzestrz@users.noreply.github.com>
2022-04-13 16:52:49 +0200

    Update temp.py

--- Machine Learning for Marketing/temp.py
+++ Machine Learning for Marketing/temp.py
@@ -5,6 +5,7 @@ Chapter 3 - Customer Lifetime Value (CLV) prediction
 '''
 import pandas as pd
 import numpy as np
+import datetime as dt
 cohort_counts=pd.read_fwf('retention.txt')
 
 # Extract cohort sizes from the first column of cohort_counts
@@ -44,3 +45,30 @@ lifespan_months = 36
 clv_granular = revenue_per_purchase * frequency_per_month * lifespan_months
 # Print granular CLV value
 print('Average granular CLV is {:.1f} USD'.format(clv_granular))
+
+# Calculate monthly spend per customer
+monthly_revenue = online.groupby(['CustomerID','InvoiceMonth'])['TotalSum'].sum().mean()
+# Calculate average monthly retention rate
+retention_rate = retention.iloc[:,1:].mean().mean()
+# Calculate average monthly churn rate
+churn_rate = 1 - retention_rate
+# Calculate traditional CLV 
+clv_traditional = monthly_revenue * (retention_rate / churn_rate)
+# Print traditional CLV and the retention rate values
+print('Average traditional CLV is {:.1f} USD at {:.1f} % retention_rate'.format(clv_traditional, retention_rate*100))
+
+# Define the snapshot date
+NOW = dt.datetime(2011,11,1)
+
+# Calculate recency by subtracting current date from the latest InvoiceDate
+features = online_X.groupby('CustomerID').agg({
+  'InvoiceDate': lambda x: (NOW - x.max()).days,
+  # Calculate frequency by counting unique number of invoices
+  'InvoiceNo': pd.Series.nunique,
+  # Calculate monetary value by summing all spend values
+  'TotalSum': np.sum,
+  # Calculate average and total quantity
+  'Quantity': ['mean', 'sum']}).reset_index()
+
+# Rename the columns
+features.columns = ['CustomerID', 'recency', 'frequency', 'monetary', 'quantity_avg', 'quantity_total']
